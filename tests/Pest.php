379f416b763e4d8db0882cf698aebfa012b1bd76
Vincent Kariuki <vincentkariuki07@gmail.com>
2025-09-21 00:34:22 +0300

    chore: Updated test directory structure, configured pest

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,9 +11,19 @@
 |
 */
 
+use Illuminate\Support\Sleep;
+
 pest()->extend(Tests\TestCase::class)
     ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature');
+    ->beforeEach(function () {
+        Str::createRandomStringsNormally();
+        Str::createUuidsNormally();
+        Http::preventStrayRequests();
+        Sleep::fake();
+
+        $this->freezeTime();
+    })
+    ->in('Browser', 'Feature', 'Unit');
 
 /*
 |--------------------------------------------------------------------------
---
+++ tests/Unit/ArchTest.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+arch()->preset()->php();
+arch()->preset()->strict();
+arch()->preset()->security();
+
+arch('controllers')
+    ->expect('App\Http\Controllers')
+    ->not->toBeUsed();
